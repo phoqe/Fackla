65b8ee901f0e502a0d4415ddc970ac00ceed786d
Linus Långberg <git@phoqe.dev>
2021-04-24 21:33:43 +0200

    feat: fix initial comp

--- app/src/main/java/com/phoqe/fackla/components/EmptyState.kt
+++ app/src/main/java/com/phoqe/fackla/components/EmptyState.kt
@@ -1,22 +1,26 @@
 package com.phoqe.fackla.components
 
-import android.annotation.TargetApi
 import android.content.Context
-import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import com.phoqe.fackla.R
-import java.security.KeyStore
+import com.phoqe.fackla.databinding.ComponentEmptyStateBinding
 
 class EmptyState @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
-    init {
-        LayoutInflater.from(context).inflate(R.layout.component_empty_state, this)
+    private val binding = ComponentEmptyStateBinding.inflate(LayoutInflater.from(context), this)
 
-        attrs?.let {
-            
+    init {
+        context.theme.obtainStyledAttributes(attrs, R.styleable.EmptyState, 0, 0).apply {
+            try {
+                binding.ivIllustration.setImageDrawable(getDrawable(R.styleable.EmptyState_image))
+                binding.tvTitle.text = getString(R.styleable.EmptyState_title)
+                binding.tvBody.text = getString(R.styleable.EmptyState_body)
+            } finally {
+                recycle()
+            }
         }
     }
 }
