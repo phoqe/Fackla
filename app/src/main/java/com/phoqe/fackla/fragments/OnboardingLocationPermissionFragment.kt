c29e06dddb050c5c8a6d45b28b3d13276da19df8
Linus Långberg <git@phoqe.dev>
2021-04-28 17:11:39 +0200

    feat: improve permission handling

--- app/src/main/java/com/phoqe/fackla/fragments/OnboardingLocationPermissionFragment.kt
+++ app/src/main/java/com/phoqe/fackla/fragments/OnboardingLocationPermissionFragment.kt
@@ -1,8 +1,12 @@
 package com.phoqe.fackla.fragments
 
 import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +15,12 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.phoqe.fackla.R
+import com.phoqe.fackla.activities.OnboardingActivity
 import com.phoqe.fackla.databinding.FragmentOnboardingLocationPermissionBinding
 
-class OnboardingLocationPermissionFragment(private val listener: OnGrantedPermissionListener) : Fragment() {
+class OnboardingLocationPermissionFragment(private val activity: OnboardingActivity) : Fragment() {
     interface OnGrantedPermissionListener {
         fun onGrantedPermission()
     }
@@ -30,14 +37,34 @@ class OnboardingLocationPermissionFragment(private val listener: OnGrantedPermis
                     requireContext(),
                     perm
             ) == PackageManager.PERMISSION_GRANTED -> {
-                listener.onGrantedPermission()
+                activity.onGrantedPermission()
             }
             shouldShowRequestPermissionRationale(perm) -> {
-                // Show info
+                showExplanationDialog()
             }
             else -> {
                 reqPermLauncher.launch(perm)
             }
+        }
+    }
+
+    private fun showExplanationDialog() {
+        context?.let {
+            MaterialAlertDialogBuilder(it)
+                    .setTitle(R.string.onboarding_location_permission_dialog_title)
+                    .setMessage(R.string.onboarding_location_permission_dialog_message)
+                    .setNegativeButton("Cancel") { dialog, which ->
+                        dialog.cancel()
+                    }
+                    .setPositiveButton("Manage") { dialog, which ->
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val data = Uri.fromParts("package", requireContext().packageName, null)
+
+                        intent.setData(data)
+
+                        startActivity(intent)
+                    }
+                    .show()
         }
     }
 
@@ -46,9 +73,9 @@ class OnboardingLocationPermissionFragment(private val listener: OnGrantedPermis
 
         reqPermLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
-                listener.onGrantedPermission()
+                activity.onGrantedPermission()
             } else {
-                // Explain
+                showExplanationDialog()
             }
         }
     }
