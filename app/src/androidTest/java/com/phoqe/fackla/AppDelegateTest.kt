5180b4c4ab9d833e943e3569e906bdb824ef44e0
Linus Långberg <git@phoqe.dev>
2021-05-14 14:29:30 +0200

    feat: sep test

--- app/src/androidTest/java/com/phoqe/fackla/AppDelegateTest.kt
+++ app/src/androidTest/java/com/phoqe/fackla/AppDelegateTest.kt
@@ -7,14 +7,13 @@ import org.junit.Assert.*
 import timber.log.Timber
 
 class AppDelegateTest {
-    private val appDelegate = AppDelegate()
+    @Test
+    fun timberPlantedTree() {
+        assertFalse(Timber.forest().isEmpty())
+    }
 
     @Test
-    fun onCreate() {
-        // Make sure Timber is correctly initialized.
-        assertFalse(Timber.forest().isEmpty())
-
-        // Mapbox should be initialized.
+    fun mapboxInit() {
         assertTrue(Mapbox.hasInstance())
         assertNotNull(Mapbox.getAccessToken())
     }
