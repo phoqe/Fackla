3530317bcc34cb1011fc48bd0c386bc4517aa310
Linus Långberg <git@phoqe.dev>
2021-05-15 10:49:13 +0200

    feat: add crawl script

---
+++ scripts/crawl.sh
@@ -0,0 +1,5 @@
+CRAWL_LAUNCHER_PATH=/Users/phoqe/Downloads/app-crawler/crawl_launcher.jar
+APK_PATH=/Users/phoqe/AndroidStudioProjects/Fackla/app/build/outputs/apk/debug/app-debug.apk
+ANDROID_SDK_PATH=/Users/phoqe/Library/Android/sdk
+
+java -jar $CRAWL_LAUNCHER_PATH --apk-file $APK_PATH --android-sdk $ANDROID_SDK_PATH
