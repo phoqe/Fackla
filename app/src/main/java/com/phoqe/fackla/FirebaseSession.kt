4f4d13a26bf302b33e42e92e29460faa0a74c27c
Linus Långberg <git@phoqe.dev>
2021-05-24 15:14:03 +0200

    feat: add appcheck

--- app/src/main/java/com/phoqe/fackla/FirebaseSession.kt
+++ app/src/main/java/com/phoqe/fackla/FirebaseSession.kt
@@ -1,7 +1,10 @@
 package com.phoqe.fackla
 
+import com.google.firebase.FirebaseApp
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
@@ -12,11 +15,12 @@ class FirebaseSession {
     private val analytics: FirebaseAnalytics = Firebase.analytics
     private val crashlytics: FirebaseCrashlytics = Firebase.crashlytics
     private val performance: FirebasePerformance = Firebase.performance
+    private val appCheck: FirebaseAppCheck = FirebaseAppCheck.getInstance()
 
     init {
         analytics.setAnalyticsCollectionEnabled(!BuildConfig.DEBUG)
         crashlytics.setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
-
         performance.isPerformanceCollectionEnabled = !BuildConfig.DEBUG
+        appCheck.installAppCheckProviderFactory(SafetyNetAppCheckProviderFactory.getInstance())
     }
 }
